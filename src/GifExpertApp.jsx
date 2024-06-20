e903a58bedf4bff57fb9b86ec87aaeed392d06c0
David-ob1 <davidvazquezt2001@gmail.com>
2024-06-20 18:03:26 -0300

    video 65

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react" 
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories ] = useState(['One Punch',"Dragon ball"])
+
+  console.log(categories)
+
   return (
    <>
     {/* titulo */}
         <h1>GifExpertApp</h1>
     {/* input */}
 
-{/* List de Gif */}
+  {/* List de Gif */}
+
+    <ol>
+      {categories.map( category =>{
+        return <li key={ category }>{ category }</li>
+
+
+      })}
+  
+
+    </ol>
+
     {/* Git Item */}
 
    </>
