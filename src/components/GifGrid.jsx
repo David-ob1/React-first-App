fad0268b966266af38120ea111e28a9dbb21080c
David-ob1 <davidvazquezt2001@gmail.com>
2024-06-25 17:18:39 -0300

    video 78

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -12,6 +12,11 @@ console.log({images,isLoading})
     return (
     <>
             <h3>{category}</h3>
+            {
+                isLoading && (<h2>Cargando...</h2>)
+            }
+
+            
             
            <div className="card-grid">
             {images.map((image) => (
